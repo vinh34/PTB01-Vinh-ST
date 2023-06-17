1189f3f940e29ac39b1e54a261dc7def93c33621
vinh34 <hoangphuvinh251013@gmail.com>
2023-06-17 20:07:59 +0700

    commit buoi 3

---
+++ Buoi_3/Buoi_3.py
@@ -0,0 +1,22 @@
+'''a=1
+b="a"
+c=False
+print(type(a))
+print(type(b))
+print(type(c))
+
+Ten=input("Tên của bạn là gì:")
+Tuoi=int(input("Bạn mấy tuổi:"))
+Lop=int(input("Bạn học lớp mấy:"))
+so_thich=input("Sở thích của bạn là gì:")
+print("Tên của bạn là:",Ten,"Tuổi của bạn là:", Tuoi,"Lớp của bạn là:", Lop, "Sở thích của bạn là:", so_thich)
+'''
+vnd=35205000/1500
+print("Số tiền 1$ là:",vnd,"vnd")
+quydoi=float(input("Bạn hãy nhập số đô la cần đổi: "))*vnd
+print("Số đô la của bạn =" ,quydoi,"vnd")
+moi_thang=1340233
+sothang=int(367329673/1340233)
+quydoi1=((sothang+1)*1340233)/vnd
+print("Số tháng kiếm được hơn 367.329.673 vnd là:", sothang+1)
+print("Số đô la của tháng hơn đó là: ", quydoi1,"$")
