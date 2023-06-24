519e8de6385af9cd4d1e89a28908b732be1683b8
vinh34 <hoangphuvinh251013@gmail.com>
2023-06-24 15:33:01 +0700

    commit btvn buoi 3

---
+++ Buoi_3/BTVN_Buoi_3.py
@@ -0,0 +1,38 @@
+do=23515
+print("1$ Mỹ là:",do,"vnd")
+quydoi1=float(input("Bạn hãy nhập số đô la mỹ cần đổi: "))*do
+print("Số đô của bạn=",quydoi1,"vnd")
+euro=25615
+print("1EUR là:",euro,"vnd")
+quydoi2=float(input("Bạn hãy nhập số Euro cần đổi: "))*euro
+print("Số euro của bạn=",quydoi2,"vnd")
+yen=164
+print("1JPY là:",yen,"vnd")
+quydoi3=float(input("Bạn hãy nhập số yên cần đổi: "))*yen
+print("Số yên của bạn=",quydoi3,"vnd")
+te=3276
+print("1 Tệ là:",te,"vnd")
+quydoi4=float(input("Bạn hãy nhập số Tệ cần đổi: "))*te
+print("Số Tệ của bạn=",quydoi4,"vnd")
+doau=15706
+print("1$ Úc là:",doau,"vnd")
+quydoi5=float(input("Bạn hãy nhập số đô la Úc cần đổi: "))*doau
+print("Số đô của bạn=",quydoi5,"vnd")
+ba=29909
+print("1 Bảng Anh là:",ba,"vnd")
+quydoi6=float(input("Bạn hãy nhập số Bảng Anh cần đổi: "))*ba
+print("Số Bảng Anh của bạn=",quydoi6,"vnd")
+won=18
+print("1 won là:",won,"vnd")
+quydoi7=float(input("Bạn hãy nhập số won cần đổi: "))*won
+print("Số đô của bạn=",quydoi7,"vnd")
+r=float(input("Bán kính của hình tròn là: "))
+pi=3.14
+cv=2*r*pi
+dt=pi*(r**2)
+print("Diện tích là:",dt,"Chu vi là:",cv)
+vdv1=float(input("Số giây về đích của vận động viên thứ nhất là:"))
+vdv2=float(input("Số giây về đích của vận động viên thứ hai là:"))
+vdv3=float(input("Số giây về đích của vận động viên thứ ba là:"))
+dtb=(vdv1+vdv2+vdv3)/3
+print("Số giây trung bình các vận động viên về đích là:",dtb)
